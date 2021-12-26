c85b20cfc2d6296df58507af452e03359ae6318c
Andrea Lepori <alepori@student.ethz.ch>
2021-12-26 21:10:33 +0100

    fix wrong filter on doctypes on server index

--- server/views.py
+++ server/views.py
@@ -53,13 +53,17 @@ def index(request):
     if request.user.is_staff:
         groups = request.user.groups.all()
 
+        q_obj = Q(group__in=groups)
+
         doc_types = DocumentType.objects.filter(
-            Q(group_private=False) | Q(group=groups[0]) & Q(enabled=True)).order_by("-id")
+            (Q(group_private=False) | q_obj) & Q(enabled=True)).order_by("-id")
     else:
         groups = request.user.groups.all()[1:]
 
+        q_obj = Q(group__in=groups)
+
         doc_types = DocumentType.objects.filter(
-            Q(group_private=False) & Q(enabled=True)).order_by("-id")
+            Q(group_private=False) & Q(enabled=True) & q_obj).order_by("-id")
 
     # check for settings
     group_check = []
